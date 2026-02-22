e4ce75d7006eb870ee244ce932073160c2282431
shaulkatz <108945729+shaulkatz@users.noreply.github.com>
2026-02-22 18:16:08 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,10 +1,3 @@
 import streamlit as st
-
-st.title("מבחן החייאה: שלב 1")
-st.write("אם אתה רואה את הטקסט הזה, השרת חי!")
-
-# בדיקה אם המפתח בכלל קיים בזיכרון
-if "GOOGLE_API_KEY" in st.secrets:
-    st.success("המפתח (Secrets) מוגדר תקין.")
-else:
-    st.warning("המפתח חסר ב-Secrets.")
+st.write("Hello World")
+st.write("If you see this, we are back in business!")
